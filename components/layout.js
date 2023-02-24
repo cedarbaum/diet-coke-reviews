2b56fb9fcbeb593af344e64ff8bb6a067b3a5b2d
Samuel Cedarbaum <scedarbaum@gmail.com>
2023-02-23 21:03:18 -0500

    Componentize search box

--- components/layout.js
+++ components/layout.js
@@ -3,6 +3,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { SearchContext } from "./SearchContext";
+import SearchBox from "./SearchBox";
 
 export default function Layout({ children }) {
   const { search, setSearch } = useContext(SearchContext);
@@ -24,40 +25,7 @@ export default function Layout({ children }) {
               </a>
             </Link>
           </div>
-          {router.pathname === "/" && (
-            <div className="flex">
-              <div
-                className="relative flex items-center h-12 rounded-lg focus-within:outline-2
-                           outline-1 outline-slate-200 outline focus-within:outline-slate-300
-                           bg-white overflow-hidden"
-              >
-                <div className="grid place-items-center h-full w-12 text-gray-300">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-6 w-6"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
-                      d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                    />
-                  </svg>
-                </div>
-                <input
-                  className="peer h-full w-full outline-none text-sm text-gray-700 pr-2"
-                  type="text"
-                  id="search"
-                  placeholder="Search something..."
-                  value={search}
-                  onChange={(e) => setSearch(e.target.value)}
-                />
-              </div>
-            </div>
-          )}
+          {router.pathname === "/" && <SearchBox />}
         </div>
       </header>
       <main className="container mx-auto flex-1">{children}</main>
