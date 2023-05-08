34053c570f1b02b73ab7cfdf1f728d0e48247b45
Samuel Cedarbaum <scedarbaum@gmail.com>
2023-05-07 22:23:53 -0400

    Fix click propagation bug

--- pages/index.js
+++ pages/index.js
@@ -84,7 +84,11 @@ export default function Home({ posts }) {
             className={`flip-card w-full h-full ${
               toggledCards.has(slug) ? "flip-card-active cursor-pointer" : ""
             }`}
-            onClick={() => (toggledCards.has(slug) ? closeCard(slug) : null)}
+            onClick={() =>
+              console.log("click", toggledCards) || toggledCards.has(slug)
+                ? closeCard(slug)
+                : undefined
+            }
           >
             <div className="card-front">
               <article className="flex flex-col translate-x-0 w-full h-full">
@@ -95,33 +99,37 @@ export default function Home({ posts }) {
                       priority={idx < NUM_IMAGES_TO_PRIORITIZE}
                     />
                   </div>
-                  <header
-                    className="grow cursor-pointer"
-                    onClick={() => openCard(slug)}
+                  <div
+                    className="grow flex flex-col cursor-pointer"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      openCard(slug);
+                    }}
                   >
-                    <div className="flex justify-between items-baseline leading-tight p-2">
-                      <h1 className="text-lg font-bold">{frontmatter.title}</h1>
-                      <p className="text-grey-darker text-sm">
-                        {frontmatter.date}
-                      </p>
-                    </div>
-                    <div className="pl-2">
-                      <h2 className="text-gray-500 text-sm">
-                        {frontmatter.neighborhood}, {frontmatter.borough}
-                      </h2>
-                    </div>
-                  </header>
-                  <footer className="p-2">
-                    <CardCanRating rating={frontmatter.rating} />
-                  </footer>
+                    <header className="grow">
+                      <div className="flex justify-between items-baseline leading-tight p-2">
+                        <h1 className="text-lg font-bold">
+                          {frontmatter.title}
+                        </h1>
+                        <p className="text-grey-darker text-sm">
+                          {frontmatter.date}
+                        </p>
+                      </div>
+                      <div className="pl-2">
+                        <h2 className="text-gray-500 text-sm">
+                          {frontmatter.neighborhood}, {frontmatter.borough}
+                        </h2>
+                      </div>
+                    </header>
+                    <footer className="p-2">
+                      <CardCanRating rating={frontmatter.rating} />
+                    </footer>
+                  </div>
                 </div>
               </article>
             </div>
-            <div
-              className="card-back cursor-pointer"
-              onClick={() => closeCard(slug)}
-            >
-              <article className="flex flex-col h-full w-full cursor-pointer">
+            <div className="card-back">
+              <article className="flex flex-col h-full w-full">
                 <div className="flex flex-col grow container">
                   <div className="grow prose p-4">
                     <header>
