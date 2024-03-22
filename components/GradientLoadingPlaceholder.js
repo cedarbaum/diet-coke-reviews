50c50a055e4dff4b7e64df9218947899d01aec7d
Samuel Cedarbaum <scedarbaum@gmail.com>
2024-03-22 00:26:23 -0400

    Add loading gradient for images

---
+++ components/GradientLoadingPlaceholder.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const GradientLoadingPlaceholder = () => {
+  return (
+    <div className="loading-indicator w-full h-full"></div>
+  );
+};
+
+export default GradientLoadingPlaceholder;
--- components/PostMedia.js
+++ components/PostMedia.js
@@ -2,9 +2,11 @@ import React from "react";
 import Image from "next/image";
 import useEmblaCarousel from "embla-carousel-react";
 import Autoplay from "embla-carousel-autoplay";
+import GradientLoadingPlaceholder from "./GradientLoadingPlaceholder";
 
 export default function PostMedia({ frontmatter, priority }) {
   const [emblaRef] = useEmblaCarousel(undefined, [Autoplay()]);
+  const [isLoaded, setIsLoaded] = React.useState(false);
 
   return (
     <div className="overflow-hidden h-full" ref={emblaRef}>
@@ -18,6 +20,11 @@ export default function PostMedia({ frontmatter, priority }) {
             key={`media_${idx}`}
             className="relative w-full h-full flex-[0_0_100%]"
           >
+            {!isLoaded && mediaType !== "video" && (
+              <div className="absolute z-50 top-0 left-0 w-full h-full">
+                <GradientLoadingPlaceholder />
+              </div>
+            )}
             {mediaType === "video" ? (
               <video
                 muted
@@ -37,6 +44,7 @@ export default function PostMedia({ frontmatter, priority }) {
                 alt={frontmatter.title}
                 src={`/${path}`}
                 priority={priority}
+                onLoad={() => setIsLoaded(true)}
                 sizes="(max-width: 768px) 100vw,
                        (max-width: 1200px) 50vw,
                        33vw"
